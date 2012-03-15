a98811967db8515512d364e80224a6f1470ed5d8
Erik Michaels-Ober <sferik@gmail.com>
2012-03-15 13:36:34 -0600

    Fresh Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mwrc2012::Application.config.secret_token = '2f3c1bb1fbaf7f46eb9a7d9afad8d7dee36b76cabee09a466d5378cfa6e32bf2da36c70f010d026d38e5231494fb91d1260f66ab7315f9877bad80b544e5e97d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mwrc2012::Application.config.session_store :cookie_store, :key => '_mwrc2012_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mwrc2012::Application.config.session_store :active_record_store
